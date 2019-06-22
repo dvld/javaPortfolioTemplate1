c7aea291d92ae19d43ff4e307bfff8b41c176151
WiseWillowDev <49727156+WiseWillowDev@users.noreply.github.com>
2019-06-22 16:32:07 -0500

    Add files via upload
    I updated the service to work

---
+++ javaPortfolioTemplate1-master/javaPortfolioTemplate1-master/src/main/java/com/tts/javaportfoliotemplate1/controllers/MainController.java
@@ -0,0 +1,49 @@
+package com.tts.javaportfoliotemplate1.controllers;
+
+import com.tts.javaportfoliotemplate1.models.Contact;
+import com.tts.javaportfoliotemplate1.repositories.ContactRepository;
+import com.tts.javaportfoliotemplate1.services.ContactService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class MainController {
+
+  @Autowired
+  private ContactRepository contactRepo;
+  
+  @Autowired
+  private ContactService contactService;
+
+	@GetMapping("/")
+	public String homePage() {
+		return "index";
+	}
+	
+	@GetMapping("/projects")
+	public String projectsPage() {
+		return "projects";
+	}
+	
+	@GetMapping("/contact")
+	public String contactPage(Contact contact) {
+  
+		return "contact";
+	}
+
+	@PostMapping("/thankyou")
+	public String thankYouPage(Contact contact, Model model) {
+    contactService.save(contact);
+    model.addAttribute("firstName", contact.getFirstName());
+    model.addAttribute("lastName", contact.getLastName());
+    model.addAttribute("message", contact.getMessage());
+    model.addAttribute("reason", contact.getReason());
+    model.addAttribute("email", contact.getEmail());
+		return "thankyou";
+	}
+
+}
---
+++ javaPortfolioTemplate1-master/javaPortfolioTemplate1-master/src/main/java/com/tts/javaportfoliotemplate1/services/ContactService.java
@@ -0,0 +1,20 @@
+ package com.tts.javaportfoliotemplate1.services;
+
+ import com.tts.javaportfoliotemplate1.models.Contact;
+ import com.tts.javaportfoliotemplate1.repositories.ContactRepository;
+
+ import org.springframework.beans.factory.annotation.Autowired;
+ import org.springframework.stereotype.Service;
+
+ @Service
+ public class ContactService {
+
+	 @Autowired
+   private ContactRepository contactRepo;
+
+   public void save(Contact contact) {
+     contactRepo.save(contact);
+   }
+
+   
+ }
