95963290edfdb818f9a932b61821ca4b9d651465
dvld <nikoroberts620@gmail.com>
2019-06-24 09:40:13 -0500

    Copied Richards changes into correct file paths; ContactService.java Contact.html ContactRepository.java MainController.java Contact .java

--- javaPortfolioTemplate1-master/javaPortfolioTemplate1-master/README.md
+++
@@ -1,4 +0,0 @@
-# Java Spring Boot Portfolio Template With Bootstrap
-
-## Overview
-This Project establishes a basic portfolio template with the use of BootStrap v4.3.1
--- src/main/java/com/tts/javaportfoliotemplate1/services/ContactService.java
+++ src/main/java/com/tts/javaportfoliotemplate1/services/ContactService.java
@@ -1,23 +1,24 @@
-// package com.tts.javaportfoliotemplate1.services;
+package com.tts.javaportfoliotemplate1.services;
 
-// import com.tts.javaportfoliotemplate1.models.Contact;
-// import com.tts.javaportfoliotemplate1.repositories.ContactRepository;
+import com.tts.javaportfoliotemplate1.models.Contact;
+import com.tts.javaportfoliotemplate1.repositories.ContactRepository;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-// @Service
-// public class ContactService {
+@Service
+public class ContactService {
 
-//   private ContactRepository contactRepo;
+  @Autowired
+  private ContactRepository contactRepo;
 
-//   @Autowired
-//   public ContactService(ContactRepository contactRepo) {
-//     this.contactRepo = contactRepo;
-//   }
+  public void save(Contact contact) {
+    contactRepo.save(contact);
+  }
 
-//   public void save(Contact contact) {
-//     contactRepo.save(contact);
-//   }
+ public Contact findByEmail(String email) {
+   return contactRepo.findByEmail(email);
+ }
 
-// }
+  
+}
